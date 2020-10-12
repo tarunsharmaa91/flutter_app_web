82682832a74609df94de1eee63664fa89244c49e
Tarun <tarun.sharma@digitaleconomysolutions.com>
2020-10-12 09:47:10 +0400

    Added text style for nav bar

--- lib/NavBar/navbar.dart
+++ lib/NavBar/navbar.dart
@@ -4,7 +4,7 @@ class NavBar extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints){
-      if(constraints.maxWidth>1200){
+      if(constraints.maxWidth>800){
         return DesktopNavBar();
       }else{
         return MobileNavBar();
@@ -36,7 +36,7 @@ class MobileNavBar extends StatelessWidget {
                   color: Colors.white
               ),
               ),
-              Text("Resume", style: TextStyle(
+              Text("Blog", style: TextStyle(
                   color: Colors.white
               ),
               )
@@ -52,33 +52,61 @@ class MobileNavBar extends StatelessWidget {
 class DesktopNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Text("Tarun", style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-            fontSize: 30
-          ),),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      child: Container(
+        constraints: BoxConstraints(maxWidth: 1200),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Tarun Sharma", style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 30
+            ),),
 
-          Row(
-            children: [
-              Text("Home", style: TextStyle(
-                color: Colors.white
-              ),
-              ),
-              Text("About Me", style: TextStyle(
+            Row(
+              children: [
+                Text("Home", style: TextStyle(
+                  fontSize: 15,
                   color: Colors.white
-              ),
-              ),
-              Text("Resume", style: TextStyle(
-                  color: Colors.white
-              ),
-              )
+                ),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                Text("About Me", style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white
+                ),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                Text("Blog", style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white
+                ),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
 
-            ],
-          )
-        ],
+                MaterialButton(
+                  color: Colors.pink,
+                    onPressed: (){},
+                  child: Text(
+                    "Get Started",
+                    style: TextStyle(
+                      color: Colors.white
+                    ),
+                  ),
+
+                    )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
